cd84684735538ce56b58f1405ffc1b790265bb92
imoz070493 <imoz070493@gmail.com>
2017-09-11 18:30:28 -0500

    Version 7.0
    Iniciando nuevo proyecto

---
+++ src/main/java/com/imoz/RecursoshumanosApplication.java
@@ -0,0 +1,12 @@
+package com.imoz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecursoshumanosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecursoshumanosApplication.class, args);
+	}
+}
